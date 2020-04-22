05be017c0cf6d79da9faafed7640cf5297f8b39e
Stephen Alfa <stephenalfa@Stephens-MacBook-Pro.local>
2020-04-21 23:01:15 -0400

    Prepare for deploy

---
+++ client/build/precache-manifest.f964586ebc6134e6e3483171d9574a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2790c8a40262b9ca17ed130339a25e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae81941e9d323b50a5c3",
+    "url": "/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "31afc59e1dbb07f5ac79",
+    "url": "/static/js/2.f2826de2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2826de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae81941e9d323b50a5c3",
+    "url": "/static/js/main.97cbe80f.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
