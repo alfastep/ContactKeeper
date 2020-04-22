d97fc61328b9fa2f5931ef43562f8f975b2ba698
Stephen Alfa <stephenalfa@Stephens-MacBook-Pro.local>
2020-04-21 22:31:08 -0400

    Fucntioning contact keeper application

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,58 @@
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
+
+export const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
+
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <a>
+        <a onClick={onLogout} href="/login">
+          <i className="fas fa-sign-out-alt" />{' '}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </a>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+    </Fragment>
+  );
+
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
+};
+
+export default Navbar;
